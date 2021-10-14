b750c33f5e8ccfcba339e00a7048b1be3be506fd
Sergey Stroganov <111sergey90@gmail.com>
2021-10-14 22:39:09 +0300

    Initial commit.

---
+++ src/main/java/org/stroganov/App.java
@@ -0,0 +1,14 @@
+package org.stroganov;
+
+/**
+ * Home library App
+ * JSON data used as Data Base!
+ * @author Sergey Stroganov
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
