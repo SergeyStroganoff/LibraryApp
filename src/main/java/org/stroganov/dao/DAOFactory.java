f6edaaefc70df7481cf805b0828463b87f21dd6e
Sergey Stroganov <111sergey90@gmail.com>
2021-11-15 21:26:56 +0300

    MySQLLibraryDAO.java class added, hibernate.cfg.xml configured.

--- src/main/java/org/stroganov/dao/DAOFactory.java
+++ src/main/java/org/stroganov/dao/DAOFactory.java
@@ -4,7 +4,7 @@ import org.stroganov.exceptions.DBExceptions;
 
 public class DAOFactory {
 
-    private static final String REALIZATION_EXCEPTION_MESSAGE = "Взаимодействие с БД H2 - не реализовано";
+    private static final String REALIZATION_EXCEPTION_MESSAGE = "Взаимодействие с БД - не реализовано";
     private static final String ERROR_PARAMETER_MESSAGE = "Тип подключения к базе указан неверно";
 
 
@@ -14,7 +14,7 @@ public class DAOFactory {
             case JSON:
                 dao = JsonLibraryDAO.getInstance();
                 break;
-            case H2DATABASE:
+            case MYSQL:
                 throw new IllegalArgumentException(REALIZATION_EXCEPTION_MESSAGE);
             default:
                 throw new IllegalArgumentException(ERROR_PARAMETER_MESSAGE);
